f553801e4b1fa4286491c87cb793451c3e1b8b08
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2024-03-31 15:12:39 -0600

    revertir cambios

--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ app.get('/cars/:carId', async (req, res) => {
 app.post('/posts', async (req, res) => {
   // Extrae los datos del post del cuerpo de la solicitud
   const {
-    titulo, contenido, fecha_publicacion, imagen_url,
+    titulo, contenido, fecha_publicacion, imagen_base64, carro_id,
   } = req.body;
 
   // Realiza validaciones básicas; puedes expandirlas según sea necesario
@@ -112,30 +112,26 @@ app.post('/posts', async (req, res) => {
   try {
     // Inserta el nuevo post en la base de datos
     const result = await conn.query(
-      'INSERT INTO Posts (titulo, contenido, fecha_publicacion, imagen_url) VALUES (?, ?, ?, ?)',
-      [titulo, contenido, fecha_publicacion, imagen_url],
+      'INSERT INTO Posts (titulo, contenido, fecha_publicacion, imagen_base64, carro_id) VALUES (?, ?, ?, ?, ?)',
+      [titulo, contenido, fecha_publicacion, imagen_base64, carro_id],
     );
 
     // Obtén el ID del nuevo post
-    const postId = result.insertId;
+    const postId = result[0].insertId;
 
     // Retorna el nuevo post; realiza otra consulta para obtener los datos recién insertados
     const [rows] = await conn.query('SELECT * FROM Posts WHERE id = ?', [postId]);
 
     if (rows.length > 0) {
-      const newPost = rows[0];
-      // Se envía la respuesta con el código 201 (Created) y se devuelve el nuevo post
-      res.status(201).json(newPost);
+      res.status(200).json(rows[0]);
     } else {
-      // Si por alguna razón no se encuentra el post recién creado, se envía un error 500
-      res.status(500).send('Error al recuperar el post recién creado');
+      res.status(404).send('Post creado pero no encontrado');
     }
   } catch (error) {
     console.error('Error al crear el post:', error);
-    // Se envía un error 500 si ocurre algún problema durante la inserción del post
     res.status(500).send('Error interno del servidor');
   }
-  writeToLog('/cars', req.body, res);
+  writeToLog('/posts', req.body, res);
   return { message: 'La función ha terminado correctamente' };
 });
 
